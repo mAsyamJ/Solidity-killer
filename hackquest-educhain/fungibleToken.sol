57a1229f177adcaff8297b17d624e60c1e5bd6da
mAsyamJ <jayanegara.asyam@gmail.com>
2025-05-11 16:55:34 +0700

    This is the last commit in NFT Educhain

--- hackquest-educhain/fungibleToken.sol
+++ hackquest-educhain/fungibleToken.sol
@@ -1,34 +1,87 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.17;
+pragma solidity ^0.8.17;
 
-contract MyToken {
-  //mapping used to store the balance corresponding to each address
-  mapping(address => uint256) private balances;
-  //A uint256 variable is used to store the total supply of the token. It is defined as public and can be queried by anyone.
-  uint256 public totalSupply;
-  //An address variable is used to store the issuer of this token. This is used for some permission control.
-  address private owner;
-
-  constructor() {
-    owner = msg.sender;
+contract MyNFT {
+  // Define a Token struct to store NFT information
+  struct Token {
+    string name; // NFT name
+    string description; // NFT description
+    address owner; // NFT owner address
   }
 
-  //function used to mint tokens
-  function mint(address recipient, uint256 amount) public {
-    //Permission control is implemented
-    require(msg.sender == owner, "Only the owner can perform this action");
-    balances[recipient] += amount;
-    totalSupply += amount;
+  // Use mapping to store the information of each NFT
+  mapping(uint256 => Token) private tokens;
+
+  mapping(address => uint256[]) private ownerTokens;
+  // Record the next available NFT ID.
+  uint256 nextTokenId = 1;
+
+  // Create an NFT function to create a new NFT and assign it to the caller.
+  function mint(string memory _name, string memory _description)
+    public
+    returns (uint256)
+  {
+    Token memory newNFT = Token(_name, _description, msg.sender);
+    tokens[nextTokenId] = newNFT;
+    ownerTokens[msg.sender].push(nextTokenId);
+    nextTokenId++;
+    return nextTokenId - 1;
   }
 
-  //A function used to query the balance corresponding to an address
-  function balanceOf(address account) public view returns (uint256) {
-    return balances[account];
+  function transfer(address _to, uint256 _tokenId) public {
+    require(_to != address(0), "Invalid recipient");
+    require(_tokenId >= 1 && _tokenId < nextTokenId, "Invalid token ID");
+    Token storage token = tokens[_tokenId];
+    require(token.owner == msg.sender, "You don't own this token");
+    token.owner = _to;
+    deleteById(msg.sender, _tokenId);
+    ownerTokens[_to].push(_tokenId);
   }
 
-  //A function used to transfer tokens.
-  function transfer(address recipient, uint256 amount) public {
-    require(amount <= balances[msg.sender], "Not enough balance.");
-    balances[msg.sender] -= amount;
+  //Create a function to get information on a specified NFT
+  function getNFT(uint256 _tokenId)
+    public
+    view
+    returns (
+      string memory name,
+      string memory description,
+      address owner
+    )
+  {
+    require(_tokenId >= 1 && _tokenId < nextTokenId, "Invalid token ID");
+    Token memory token = tokens[_tokenId];
+
+    name = token.name;
+    description = token.description;
+    owner = token.owner;
+  }
+
+  // Get all NFT IDs owned by a specified address
+
+  function getTokensByOwner(address _owner)
+    public
+    view
+    returns (uint256[] memory)
+  {
+    return ownerTokens[_owner];
+  }
+
+  function deleteById(address account, uint256 _tokenId) internal {
+    uint256[] storage ownerTokenList = ownerTokens[account];
+    for (uint256 i = 0; i < ownerTokenList.length; i++) {
+      if (ownerTokenList[i] == _tokenId) {
+        ownerTokenList[i] = ownerTokenList[ownerTokenList.length - 1];
+        ownerTokenList.pop();
+        break;
+      }
+    }
+  }
+
+  function burn(uint256 _tokenId) public {
+    require(_tokenId >= 1 && _tokenId < nextTokenId, "Invalid Token ID");
+    Token storage token = tokens[_tokenId];
+    require(token.owner == msg.sender, "You don't own this token");
+    deleteById(msg.sender, _tokenId);
+    delete tokens[_tokenId];
   }
 }
