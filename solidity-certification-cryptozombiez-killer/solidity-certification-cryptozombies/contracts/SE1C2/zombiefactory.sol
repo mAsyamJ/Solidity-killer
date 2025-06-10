19c5107beca5d149ba59ff833863db6a9de3391d
mAsyamJ <jayanegara.asyam@gmail.com>
2025-06-11 00:14:00 +0700

    zombiehelper.sol

--- solidity-certification-cryptozombiez-killer/solidity-certification-cryptozombies/contracts/SE1C2/zombiefactory.sol
+++ solidity-certification-cryptozombiez-killer/solidity-certification-cryptozombies/contracts/SE1C2/zombiefactory.sol
@@ -1,14 +1,21 @@
 pragma solidity >=0.5.0 <0.6.0;
 
-contract ZombieFactory {
+import "./ownable.sol";
+
+contract ZombieFactory is Ownable {
     event NewZombie(uint zombieId, string name, uint dna);
 
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
+    uint cooldownTime = 1 days;
 
     struct Zombie {
         string name;
         uint dna;
+        uint32 level;
+        uint32 readyTime;
+        uint16 winCount;
+        uint16 lossCount;
     }
 
     Zombie[] public zombies;
@@ -17,7 +24,9 @@ contract ZombieFactory {
     mapping(address => uint) ownerZombieCount;
 
     function _createZombie(string memory _name, uint _dna) internal {
-        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        uint id = zombies.push(
+            Zombie(_name, _dna, 1, uint32(now + cooldownTime), 0, 0)
+        ) - 1;
         zombieToOwner[id] = msg.sender;
         ownerZombieCount[msg.sender]++;
         emit NewZombie(id, _name, _dna);
